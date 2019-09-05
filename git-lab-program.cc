3835dbfb5dc6db03bc1233af57d5a63cc52d1eff
Cory Zink <cz109918@ohio.edu>
2019-09-05 16:25:08 -0400

    new program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Cory Zink
+ *        Date: 9/5/19
+ * Description: Writes words "Hello Git!!"
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
